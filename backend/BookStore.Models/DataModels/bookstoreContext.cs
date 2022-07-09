77118767c9f3a921a8277dd49d6cb1a3d9afb17c
Akshay Thummar <akshaythummar90@gmail.com>
2022-07-09 18:19:25 +0530

    gitignore added

--- backend/BookStore.Models/DataModels/bookstoreContext.cs
+++ backend/BookStore.Models/DataModels/bookstoreContext.cs
@@ -31,7 +31,7 @@ namespace BookStore.Models.DataModels
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Server=localhost;port=5432;Database=bookstore;User Id=postgres;Password=Vgy123$.;SSL Mode=Prefer");
+                optionsBuilder.UseNpgsql("Server=localhost;port=5432;Database=bookstore;User Id=postgres;Password=*********;SSL Mode=Prefer");
             }
         }
 
